8d8b5bf3fca9ec571282eea0383f3a3669484e93
Taco van Dijk <taco@waag.org>
2021-02-12 14:20:12 +0100

    optionality disable button

--- Demonstrator/ResearchInterface/genecoop/static/genecoop/user.js
+++ Demonstrator/ResearchInterface/genecoop/static/genecoop/user.js
@@ -1,5 +1,29 @@
-//toggle visibility of li in permissions, based on agreements
+//on agreement-change toggle the selected permissions, 
+//and enable submit button when allowed by optionality 
 function onAgreementChange(e)
+{
+    togglePermissions(e);
+    checkOptionality();
+}
+
+//enable button only when all mandatory fields are yes
+function checkOptionality()
+{
+    let button = document.getElementById("submitButton");
+    var mandatory_options = document.querySelectorAll('.mandatory');
+    let checked = Array.from(mandatory_options).map(item =>{
+        //the yes options is the one of which the value ends with 0000, there is only one.
+        let yes = Array.from(item.getElementsByTagName('input'))
+            .filter(el => el.value.endsWith("0000"))[0];
+        return yes.checked;
+    });
+
+    let allowed = checked.reduce((res,cur) => res && cur, true);
+    button.disabled = !allowed;
+}
+
+//toggle visibility of li in permissions, based on agreements
+function togglePermissions(e)
 {
     var selected = e.target.value.endsWith('0000');//yes value
     var selector = '.op_' + e.target.id;
